48a0aa17809bab83a64ebfd57e44f48c5394f8f5
peterdumbari <dummytwiz@gmail.com>
2022-12-07 17:44:06 +0100

    updated team page

--- src/Pages/Teams/Teams.jsx
+++ src/Pages/Teams/Teams.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import "../Teams/Teams.scss";
+import { Link } from 'react-router-dom';
 
 export default function Teams() {
   return (
-    <div>Teams</div>
+    <div>Teams
+      <Link to="/upload">?</Link>
+    </div>
   )
 }
